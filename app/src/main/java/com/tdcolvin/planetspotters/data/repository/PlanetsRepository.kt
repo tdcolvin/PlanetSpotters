1b1f01cbdad49bbdb8cd5623cbc4027ad089f4ab
Tom Colvin <tdcolvin@gmail.com>
2023-06-15 21:27:02 +0100

    Our completed PlanetsRepository interface

--- app/src/main/java/com/tdcolvin/planetspotters/data/repository/PlanetsRepository.kt
+++ app/src/main/java/com/tdcolvin/planetspotters/data/repository/PlanetsRepository.kt
@@ -1,5 +1,9 @@
 package com.tdcolvin.planetspotters.data.repository
 
+import kotlinx.coroutines.flow.Flow
+
 interface PlanetsRepository {
+    fun getPlanets(): Flow<List<Planet>>
     suspend fun addPlanet(planet: Planet)
+    suspend fun deletePlanet(planetId: String)
 }
