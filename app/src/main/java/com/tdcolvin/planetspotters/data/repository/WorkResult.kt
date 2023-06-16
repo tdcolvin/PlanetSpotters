f3d51d4fa3c8a377f94346253ac4959c98d4604d
Tom Colvin <tdcolvin@gmail.com>
2023-06-16 14:50:53 +0100

    Updated to support a loading state

--- app/src/main/java/com/tdcolvin/planetspotters/data/repository/WorkResult.kt
+++ app/src/main/java/com/tdcolvin/planetspotters/data/repository/WorkResult.kt
@@ -3,4 +3,5 @@ package com.tdcolvin.planetspotters.data.repository
 sealed class WorkResult<out R> {
     data class Success<out T>(val data: T) : WorkResult<T>()
     data class Error(val exception: Exception) : WorkResult<Nothing>()
+    object Loading : WorkResult<Nothing>()
 }
