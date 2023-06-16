ec2fc922ee9ffaa0821f5bd7b958069f9e06ce45
Tom Colvin <tdcolvin@gmail.com>
2023-06-16 16:18:57 +0100

    Added a main activity

---
+++ app/src/main/java/com/tdcolvin/planetspotters/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tdcolvin.planetspotters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
