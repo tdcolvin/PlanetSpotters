a48213ee4d749cded8300be4a5382459bbdd504b
Tom Colvin <tdcolvin@gmail.com>
2023-06-18 09:10:20 +0100

    Default ID of null

--- app/src/main/java/com/tdcolvin/planetspotters/data/repository/Planet.kt
+++ app/src/main/java/com/tdcolvin/planetspotters/data/repository/Planet.kt
@@ -3,7 +3,7 @@ package com.tdcolvin.planetspotters.data.repository
 import java.util.Date
 
 data class Planet(
-    val planetId: String?,
+    val planetId: String? = null,
     val name: String,
     val distanceLy: Float,
     val discovered: Date
